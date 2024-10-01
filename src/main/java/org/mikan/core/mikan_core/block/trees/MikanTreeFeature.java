0e4e09534ddfeaa03a82236a79e70367afcd3749
Mikan_1 <ichi19721972@gmail.com>
2024-10-01 17:21:53 +0900

    ブロック追加

--- src/main/java/org/mikan/core/mikan_core/block/trees/MikanTreeFeature.java
+++ src/main/java/org/mikan/core/mikan_core/block/trees/MikanTreeFeature.java
@@ -1,8 +1,12 @@
 package org.mikan.core.mikan_core.block.trees;
 
 import com.mojang.serialization.Codec;
+import jdk.nashorn.internal.ir.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.trees.Tree;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IWorld;
 import net.minecraft.world.gen.IWorldGenerationBaseReader;
 import net.minecraft.world.gen.blockstateprovider.SimpleBlockStateProvider;
 import net.minecraft.world.gen.feature.BaseTreeFeatureConfig;
@@ -10,22 +14,28 @@ import net.minecraft.world.gen.feature.ConfiguredFeature;
 import net.minecraft.world.gen.feature.TreeFeature;
 import org.mikan.core.mikan_core.init.MikanCoreBlocks;
 
+import java.util.Random;
+
 public class MikanTreeFeature extends TreeFeature {
 
-    public MikanTreeFeature(Codec<BaseTreeFeatureConfig> p_i231999_1_) {
-        super(p_i231999_1_);
+    public MikanTreeFeature(Codec<BaseTreeFeatureConfig> codec){
+        super(codec);
+
     }
 
-    public static boolean isFree(IWorldGenerationBaseReader p_236410_0_, BlockPos p_236410_1_) {
-        return validTreePos(p_236410_0_, p_236410_1_) || p_236410_0_.isStateAtPosition(p_236410_1_, (p_236417_0_) -> {
-            return p_236417_0_.is(MikanCoreBlocks.Register.MIKAN_LOG.get());
-        });
+    protected boolean BaseTreeFeatureConfig(IWorld world, Random random, BlockPos pos, BaseTreeFeatureConfig config){
+        for(int y = 0;y<5; y++){
+            world.setBlock(pos.above(y), MikanCoreBlocks.Register.MIKAN_LOG.get().defaultBlockState(), 3);
+        }
+        for (int y = 3; y <= 5; y++) {
+            for (int x = -2; x <= 2; x++) {
+                for (int z = -2; z <= 2; z++) {
+                    if (Math.abs(x) + Math.abs(z) <= 2) {
+                        world.setBlock(pos.offset(x, y, z), MikanCoreBlocks.Register.MIKAN_LOG.get().defaultBlockState(), 3);
+                    }
+                }
+            }
+        }
+        return true;
     }
-
-    public static boolean isAirOrLeaves(IWorldGenerationBaseReader p_236412_0_, BlockPos p_236412_1_) {
-        return p_236412_0_.isStateAtPosition(p_236412_1_, (p_236411_0_) -> {
-            return p_236411_0_.isAir() || p_236411_0_.is(BlockTags.LEAVES);
-        });
-    }
-
 }
