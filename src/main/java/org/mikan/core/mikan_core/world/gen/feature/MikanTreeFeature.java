6a7bcf50020a762bd70e0a0de1f0b6f366c8fe0f
青袋 <102774735+AOBUKURO@users.noreply.github.com>
2024-10-02 23:15:08 +0900

    木の生成

--- src/main/java/org/mikan/core/mikan_core/world/gen/feature/MikanTreeFeature.java
+++ src/main/java/org/mikan/core/mikan_core/world/gen/feature/MikanTreeFeature.java
@@ -1,17 +1,39 @@
 package org.mikan.core.mikan_core.world.gen.feature;
 
 import com.mojang.serialization.Codec;
-import net.minecraft.world.gen.feature.BaseTreeFeatureConfig;
-import net.minecraft.world.gen.feature.TreeFeature;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MutableBoundingBox;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.WorldGenRegistries;
+import net.minecraft.world.ISeedReader;
+import net.minecraft.world.gen.blockstateprovider.SimpleBlockStateProvider;
+import net.minecraft.world.gen.feature.*;
+import net.minecraft.world.gen.foliageplacer.BlobFoliagePlacer;
+import net.minecraft.world.gen.trunkplacer.StraightTrunkPlacer;
+import org.mikan.core.mikan_core.init.MikanCoreBlocks;
 
-public class MikanTreeFeature extends TreeFeature {
+import java.util.Random;
+import java.util.Set;
 
-    public MikanTreeFeature(Codec<BaseTreeFeatureConfig> codec) {
-        super(codec);
+public class MikanTreeFeature  {
+
+
+    public static final ConfiguredFeature<BaseTreeFeatureConfig, ?> MIKAN = register(
+            "mikan",
+            Feature.TREE.configured((new BaseTreeFeatureConfig.Builder
+                    (new SimpleBlockStateProvider(MikanCoreBlocks.Register.MIKAN_LOG.get().defaultBlockState()),
+                            new SimpleBlockStateProvider(MikanCoreBlocks.Register.MIKAN_LEAVE.get().defaultBlockState()),
+                            new BlobFoliagePlacer(FeatureSpread.fixed(2),
+                                    FeatureSpread.fixed(0), 3),
+                            new StraightTrunkPlacer(5, 2, 0),
+                            new TwoLayerFeature(1, 0, 1))).ignoreVines().build()));
+
+    private static <FC extends IFeatureConfig> ConfiguredFeature<FC, ?> register(String p_243968_0_, ConfiguredFeature<FC, ?> p_243968_1_) {
+        return Registry.register(WorldGenRegistries.CONFIGURED_FEATURE, p_243968_0_, p_243968_1_);
+    }
     }
 
 
 
 
 
-}
