ee548525475f81b2a4ba7458c52e84a4ede35322
青袋 <102774735+AOBUKURO@users.noreply.github.com>
2024-10-12 23:12:11 +0900

    アイテム登録とレシピ登録

--- src/main/java/org/mikan/core/mikan_core/init/MikanCoreItems.java
+++ src/main/java/org/mikan/core/mikan_core/init/MikanCoreItems.java
@@ -63,7 +63,39 @@ public class MikanCoreItems {
 
         }
         //参照用登録
-        public static final RegistryObject<Item> MIKAN = RegistryObject.of(new ResourceLocation("mikan_core:mikan"), ForgeRegistries.ITEMS);
-        public static final RegistryObject<Item> ASTIUM_GEM = RegistryObject.of(new ResourceLocation("mikan_core:astium_gem"), ForgeRegistries.ITEMS);
+        public static final RegistryObject<Item> ASTIUM_GEM = RegistryObject.of(new ResourceLocation("mikan_core:astium_gem"),ForgeRegistries.ITEMS);
+        public static final RegistryObject<Item> ASTIUM_POWDER = RegistryObject.of(new ResourceLocation("mikan_core:astium_powder"),ForgeRegistries.ITEMS);
+        public static final RegistryObject<Item> DIAMOND_GEAR = RegistryObject.of(new ResourceLocation("mikan_core:diamond_gear"),ForgeRegistries.ITEMS);
+        public static final RegistryObject<Item> DIAMOND_SHAVES = RegistryObject.of(new ResourceLocation("mikan_core:diamond_shaves"),ForgeRegistries.ITEMS);
+        public static final RegistryObject<Item> ENDERLIGHT_GEM = RegistryObject.of(new ResourceLocation("mikan_core:enderlight_gem"),ForgeRegistries.ITEMS);
+        public static final RegistryObject<Item> ENDERLIGHT_POWDER = RegistryObject.of(new ResourceLocation("mikan_core:enderlight_powder"),ForgeRegistries.ITEMS);
+        public static final RegistryObject<Item> FORCINIUM_GEM = RegistryObject.of(new ResourceLocation("mikan_core:forcinium_gem"),ForgeRegistries.ITEMS);
+        public static final RegistryObject<Item> FORCINIUM_POWDER = RegistryObject.of(new ResourceLocation("mikan_core:forcinium_powder"),ForgeRegistries.ITEMS);
+        public static final RegistryObject<Item> FORNEUS_INGOT = RegistryObject.of(new ResourceLocation("mikan_core:forneus_ingot"),ForgeRegistries.ITEMS);
+        public static final RegistryObject<Item> FORNEUS_POWDER = RegistryObject.of(new ResourceLocation("mikan_core:forneus_powder"),ForgeRegistries.ITEMS);
+        public static final RegistryObject<Item> GOLD_GEAR = RegistryObject.of(new ResourceLocation("mikan_core:gold_gear"),ForgeRegistries.ITEMS);
+        public static final RegistryObject<Item> HAND_COMPRESSOR = RegistryObject.of(new ResourceLocation("mikan_core:hand_compressor"),ForgeRegistries.ITEMS);
+        public static final RegistryObject<Item> HELLUS_INGOT = RegistryObject.of(new ResourceLocation("mikan_core:hellus_ingot"),ForgeRegistries.ITEMS);
+        public static final RegistryObject<Item> IRON_GEAR = RegistryObject.of(new ResourceLocation("mikan_core:iron_gear"),ForgeRegistries.ITEMS);
+        public static final RegistryObject<Item> MACHINE_HUMMER = RegistryObject.of(new ResourceLocation("mikan_core:machine_hummer"),ForgeRegistries.ITEMS);
+        public static final RegistryObject<Item> MANUAL_MILL = RegistryObject.of(new ResourceLocation("mikan_core:manual_mill"),ForgeRegistries.ITEMS);
+        public static final RegistryObject<Item> MIKAN = RegistryObject.of(new ResourceLocation("mikan_core:mikan"),ForgeRegistries.ITEMS);
+        public static final RegistryObject<Item> MIKAN_JUICE_100 = RegistryObject.of(new ResourceLocation("mikan_core:mikan_juice_100"),ForgeRegistries.ITEMS);
+        public static final RegistryObject<Item> MIKAN_JUICE_800 = RegistryObject.of(new ResourceLocation("mikan_core:mikan_juice_800"),ForgeRegistries.ITEMS);
+        public static final RegistryObject<Item> MIKAN_JUICE_6400 = RegistryObject.of(new ResourceLocation("mikan_core:mikan_juice_6400"),ForgeRegistries.ITEMS);
+        public static final RegistryObject<Item> MIKAN_TORCH = RegistryObject.of(new ResourceLocation("mikan_core:mikan_torch"),ForgeRegistries.ITEMS);
+        public static final RegistryObject<Item> MIKANIUM_INGOT = RegistryObject.of(new ResourceLocation("mikan_core:mikanium_ingot"),ForgeRegistries.ITEMS);
+        public static final RegistryObject<Item> MINERAL_PRESSURIZED_CONCENTRATOR = RegistryObject.of(new ResourceLocation("mikan_core:mineral_pressurized_concentrator"),ForgeRegistries.ITEMS);
+        public static final RegistryObject<Item> NERUMES_INGOT = RegistryObject.of(new ResourceLocation("mikan_core:nerumes_ingot"),ForgeRegistries.ITEMS);
+        public static final RegistryObject<Item> NERUMES_POWDER = RegistryObject.of(new ResourceLocation("mikan_core:nerumes_powder"),ForgeRegistries.ITEMS);
+        public static final RegistryObject<Item> PARCING_NAKE_INGOT = RegistryObject.of(new ResourceLocation("mikan_core:parcing_nake_ingot"),ForgeRegistries.ITEMS);
+        public static final RegistryObject<Item> PARCING_POWDER = RegistryObject.of(new ResourceLocation("mikan_core:parcing_powder"),ForgeRegistries.ITEMS);
+        public static final RegistryObject<Item> STONE_GEAR = RegistryObject.of(new ResourceLocation("mikan_core:stone_gear"),ForgeRegistries.ITEMS);
+        public static final RegistryObject<Item> WOOD_GEAR = RegistryObject.of(new ResourceLocation("mikan_core:wood_gear.json"),ForgeRegistries.ITEMS);
+        public static final RegistryObject<Item> ZEOID_INGOT = RegistryObject.of(new ResourceLocation("mikan_core:zeoid_ingot"),ForgeRegistries.ITEMS);
+        public static final RegistryObject<Item> ZEOID_POWDER = RegistryObject.of(new ResourceLocation("mikan_core:zeoid_powder"),ForgeRegistries.ITEMS);
+
+
+
     }
 }
--- src/main/java/org/mikan/core/mikan_core/item/ItemMachineHummer.java
+++ src/main/java/org/mikan/core/mikan_core/item/ItemMachineHummer.java
@@ -1,6 +1,8 @@
 package org.mikan.core.mikan_core.item;
 
+import net.minecraft.block.BlockState;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import org.mikan.core.mikan_core.init.MikanTabinit;
 
 public class ItemMachineHummer extends Item {
@@ -9,5 +11,29 @@ public class ItemMachineHummer extends Item {
         super(new Properties().tab(MikanTabinit.Mikan_tab).durability(60));
         this.setRegistryName("machine_hummer");
 
+    }
+    @Override
+    public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+        return 1F;
+    }
+    //修復不可
+    @Override
+    public boolean isRepairable(ItemStack itemstack) {
+        return false;
+    }
+    //クラフトアイテム残る
+    @Override
+    public boolean hasCraftingRemainingItem() {
+        return true;
+    }
+    //耐久値を消費
+    @Override
+    public ItemStack getContainerItem(ItemStack itemstack) {
+        ItemStack retval = new ItemStack(this);
+        retval.setDamageValue(itemstack.getDamageValue() + 1);
+        if (retval.getDamageValue() >= retval.getMaxDamage()) {
+            return ItemStack.EMPTY;
+        }
+        return retval;
     }
 }
