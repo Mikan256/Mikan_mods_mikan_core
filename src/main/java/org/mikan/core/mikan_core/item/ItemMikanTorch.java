39f6bc23859ce5615bcc6e6a4ade804ecc480b31
青袋 <102774735+AOBUKURO@users.noreply.github.com>
2024-10-13 13:25:45 +0900

    レシピ登録と正式版開発

--- src/main/java/org/mikan/core/mikan_core/item/ItemMikanTorch.java
+++ src/main/java/org/mikan/core/mikan_core/item/ItemMikanTorch.java
@@ -1,6 +1,16 @@
 package org.mikan.core.mikan_core.item;
 
+import net.minecraft.command.impl.EffectCommand;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Effect;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.LogicalSidedProvider;
 import org.mikan.core.mikan_core.init.MikanTabinit;
 
 public class ItemMikanTorch extends Item {
@@ -9,4 +19,16 @@ public class ItemMikanTorch extends Item {
         super(new Properties().tab(MikanTabinit.Mikan_tab));
         this.setRegistryName("mikan_torch");
     }
+    @Override
+    public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
+        if (!world.isClientSide && entity instanceof  PlayerEntity){
+            PlayerEntity player = (PlayerEntity) entity;
+            player.clearFire();
+            player.addEffect(new EffectInstance(Effects.SATURATION,20,0,false,false));
+
+        }
+        super.inventoryTick(stack, world, entity, slot, selected);
+
+    }
+
 }
