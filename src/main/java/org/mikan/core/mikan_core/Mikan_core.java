e26d84990d3c9f5c21904df858b6c69d41e486c5
青袋 <102774735+AOBUKURO@users.noreply.github.com>
2024-10-13 13:25:45 +0900

    レシピ登録と正式版開発

--- src/main/java/org/mikan/core/mikan_core/Mikan_core.java
+++ src/main/java/org/mikan/core/mikan_core/Mikan_core.java
@@ -1,7 +1,5 @@
 package org.mikan.core.mikan_core;
 
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
